471df37548f2e7fb73b4e3ac1a33f71a64654ae7
Lakshmilaks <lakshmilaksh2507@gmail.com>
2025-05-23 17:43:54 +0530

    Handled exception

--- src/main/java/com/lakshmi/dice/Simulator.java
+++ src/main/java/com/lakshmi/dice/Simulator.java
@@ -2,30 +2,48 @@ package com.lakshmi.dice;
 
 import java.util.Random;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 public class Simulator {
 
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("How many dice would you like to roll?");
-		int numOfDice = sc.nextInt();
-		System.out.println("About to roll "+numOfDice+" dice.");
-//		Random rand = new Random();
-//		int rollNumber = rand.nextInt(6)+1;
-//		System.out.println(display(rollNumber));
+		try {
+			System.out.println("How many dice would you like to roll?");
+			int numOfDice = sc.nextInt();
+			System.out.println("About to roll "+numOfDice+" dice.");
+			
+			Random rand = new Random();
+			
+			for(int i = 0; i < numOfDice; i++) {
+				int rollNumber = rand.nextInt(6)+1;
+				System.out.println(display(rollNumber));
+			}
+		}catch(InputMismatchException e) {
+			System.out.println("This is not a valid number!");
+		}
 		
 	}
 	
 	static String display(int value) {
 		switch(value) {
-		case 1: return "---------\n|       |\n|    o  |\n|       |\n---------";
+		case 1: return "---------\n|       |\n|   o   |\n|       |\n---------";
 		case 2: return "---------\n| o     |\n|       |\n|     o |\n---------";
 		case 3: return "---------\n| o     |\n|   o   |\n|     o |\n---------";
 		case 4: return "---------\n| o   o |\n|       |\n| o   o |\n---------";
-		case 5: return "---------\n| o   o |\n|    o  |\n| o   o |\n---------";
+		case 5: return "---------\n| o   o |\n|   o   |\n| o   o |\n---------";
 		case 6: return "---------\n| o   o |\n| o   o |\n| o   o |\n---------";
 		default:
 			return "no matched dice";
 		}
+//		return switch(value) {
+//		case 1 -> "---------\n|       |\n|    o  |\n|       |\n---------";
+//		case 2 -> "---------\n| o     |\n|       |\n|     o |\n---------";
+//		case 3 -> "---------\n| o     |\n|   o   |\n|     o |\n---------";
+//		case 4 -> "---------\n| o   o |\n|       |\n| o   o |\n---------";
+//		case 5 -> "---------\n| o   o |\n|    o  |\n| o   o |\n---------";
+//		case 6 -> "---------\n| o   o |\n| o   o |\n| o   o |\n---------";
+//		default -> "no matched dice";
+//		}; using enhanced switch
 	}
 }
